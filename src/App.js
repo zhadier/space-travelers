1cf79c78acf44f63e0c11e7584715ca9978545d2
andres-condezo <ra.condezomonge@gmail.com>
2022-02-28 17:04:06 -0500

    Feat(app.js): [Import components]

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/configureStore';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import store from './redux/configureStore';
 import Header from './components/Header';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import Profile from './components/Profile';
+import NotFound from './components/NotFound';
 
 const App = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Header />
-      {/* <Routes>
-        <Route path="/" element={} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes> */}
-    </BrowserRouter>
-  </Provider>
+  // <Provider store={store}>
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Rockets />} />
+      <Route path="/missions" element={<Missions />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+  // </Provider>
 );
 
 export default App;
