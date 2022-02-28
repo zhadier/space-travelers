d1d5a027ccc716d433c5154ebd61943a7aa83c53
Zeeshan Haider <zhadier39@gmail.com>
2022-03-01 01:18:20 +0500

    fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import Header from './components/Header';
-import BookListContainer from './components/BooklistContainer';
-import Categories from './components/Categories';
 
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<BookListContainer />} />
+      {/* <Routes>
+        <Route path="/" element={} />
         <Route path="/categories" element={<Categories />} />
-      </Routes>
+      </Routes> */}
     </BrowserRouter>
   </Provider>
 );
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,13 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import booksReducer, { getBooksFromAPI } from './books/books';
+// import { createStore, combineReducers, applyMiddleware } from 'redux';
+// import logger from 'redux-logger';
+// import thunk from 'redux-thunk';
 
-const reducer = combineReducers({
-  booksReducer,
-  // additional reducers could be added here
-});
+// const reducer = combineReducers({
+//   booksReducer,
+//   // additional reducers could be added here
+// });
 
-const store = createStore(reducer, applyMiddleware(thunk, logger));
-store.dispatch(getBooksFromAPI());
+// const store = createStore(reducer, applyMiddleware(thunk, logger));
+// store.dispatch(getBooksFromAPI());
 
-export default store;
+// export default store;
