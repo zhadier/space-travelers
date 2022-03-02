cba2b3601515633aaf1532f3b31b593b729f8cea
andres-condezo <ra.condezomonge@gmail.com>
2022-03-02 10:47:52 -0500

    Create the reservedMission section

---
+++ src/components/ReservedMissions.js
@@ -0,0 +1,34 @@
+import { useDispatch } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { leaveMission } from '../redux/missions/missions';
+
+const ReservedMission = (props) => {
+  const dispatch = useDispatch();
+  const { id, name, wikipedia } = props;
+
+  const handleLeaveMission = () => {
+    dispatch(leaveMission(id));
+  };
+
+  console.log(wikipedia, 'este');
+
+  return (
+    <li className="reserved__container" id={id}>
+      <h3>{name}</h3>
+      <div className="reserved__buttons">
+        <a href={wikipedia} target="_blank" rel="noreferrer">Read more</a>
+        <button type="button" className="remove-reservation" onClick={handleLeaveMission}>
+          Leave Mission
+        </button>
+      </div>
+    </li>
+  );
+};
+
+ReservedMission.propTypes = {
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  wikipedia: PropTypes.string.isRequired,
+};
+
+export default ReservedMission;
