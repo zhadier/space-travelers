0fb81d0bb073fe6c2c433ca02fc362712267e2a2
andres-condezo <ra.condezomonge@gmail.com>
2022-03-02 09:35:03 -0500

    Delete tha dispatch to Missions function

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,16 +2,14 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rocketsReducer, { getRocketsFromAPI } from './rockets/rockets';
-import missionsReducer, { fetchMissionsFromAPI } from './missions/missions';
+import missionsReducer from './missions/missions';
 
 const reducer = combineReducers({
   missionsReducer,
   rocketsReducer,
-  // additional reducers could be added here
 });
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 store.dispatch(getRocketsFromAPI());
-store.dispatch(fetchMissionsFromAPI());
 
 export default store;
